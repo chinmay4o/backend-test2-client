a58ca04c89e9551531c545254de2f0218292ccc6
surve4404 <surve4404@gmail.com>
2021-07-21 23:08:57 +0530

    navbar changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,7 +10,7 @@ const Nav = () => {
       >
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
-           <h2>Navbar</h2> 
+           <h2>TinyUrl</h2> 
           </a>
           <button
             className="navbar-toggler"
@@ -23,33 +23,33 @@ const Nav = () => {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse mr-auto" id="navbarNav">
-            <ul className="navbar-nav mx-auto pl-2">
-              <li className="nav-item pl-2">
+          <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item  pe-3">
                 <Link className="nav-link" to="/">
                   {" "}
                   Home
                 </Link>
               </li>
-              <li className="nav-item pl-2">
+              <li className="nav-item pe-3">
                 <Link className="nav-link" to="/about">
                   {" "}
                   About
                 </Link>
               </li>
-              <li className="nav-item pl-3">
+              <li className="nav-item pe-3">
                 <Link className="nav-link" to="/login">
                   {" "}
                   Login
                 </Link>
               </li>
-              <li className="nav-item">
+              <li className="nav-item pe-3">
                 <Link className="nav-link" to="/register">
                   {" "}
                   Register
                 </Link>
               </li>
-              <li className="nav-item">
+              <li className="nav-item pe-3">
                 <Link className="nav-link" to="/contact">
                   {" "}
                   Contact Us
